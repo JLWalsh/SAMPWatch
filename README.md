d8fde28ab0266f267343e8a264e524675784bf41
James L. Walsh <jloubierwalsh@gmail.com>
2023-03-03 19:44:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SAMP Watch
+
+A simplistic live reload tool that restarts your [open.mp](https://www.open.mp/) server when it detects changes
+
+## Install
+
+1. Install watchdog through pip
+```
+pip install watchdog
+```
+
+2. Configure `.sampwatch` to your desired settings. An example .sampwatch config file is available in this repository
+
+3. Run SAMP Watch!
+```
+./watch.py
+```
---
+++ watch.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+import configparser
+import subprocess
+import time
+from threading import Timer
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+CONFIG_FILE_NAME = '.sampwatch'
+
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_NAME)
+
+    server_dir = config['server']['directory']
+    watch_dir = config['watcher']['directory']
+
+    files = config['watcher']['files'].split(',')
+
+    return {'server_dir': server_dir, 'watch_dir': watch_dir, 'files': files}
+
+# Taken from https://gist.github.com/walkermatt/2871026
+def debounce(wait):
+    """ Decorator that will postpone a functions
+        execution until after wait seconds
+        have elapsed since the last time it was invoked. """
+    def decorator(fn):
+        def debounced(*args, **kwargs):
+            def call_it():
+                fn(*args, **kwargs)
+            try:
+                debounced.t.cancel()
+            except(AttributeError):
+                pass
+            debounced.t = Timer(wait, call_it)
+            debounced.t.start()
+        return debounced
+    return decorator
+
+def print_header(message):
+    print(f'---------- {message} ----------')
+
+def start_server(server_dir):
+    print_header('STARTING SERVER')
+    process = subprocess.Popen([f"exec {server_dir}/omp-server"], shell=True)
+    print(f'SERVER STARTED ON PID {process.pid}')
+    return process
+
+@debounce(1)
+def restart_server(server_process, config):
+    print_header('STOPPING SERVER')
+    server_process.kill()
+    return start_server(config['server_dir'])
+
+def start_file_watcher(process, config):
+    event_handler = FileChangedHandler(process, config)
+    observer = Observer()
+    observer.schedule(event_handler, path=config['watch_dir'], recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+class FileChangedHandler(FileSystemEventHandler):
+    server_process = None
+    config = None
+
+    def __init__(self, server_process, config):
+        self.server_process = server_process
+        self.config = config
+
+    def on_modified(self, event):
+        if event.event_type == 'modified':
+            filename = event.src_path[len(self.config['watch_dir']) + 1:]
+
+            if filename in self.config['files']:
+                maybe_new_process = restart_server(self.server_process, self.config)
+                if maybe_new_process is not None:
+                    self.server_process = maybe_new_process
+
+if __name__ == "__main__":
+    config = read_config();
+
+    process = start_server(config['server_dir'])
+    start_file_watcher(process, config)
+
